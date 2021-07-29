a721da15159a4e612a81567144527f90779dd84f
Akash Kumar Pal <akashpal@drreddys.com>
2021-07-29 10:58:09 +0530

    Removing unwanted imports

--- swagger.demo/src/main/java/com/springboot/swagger/demo/controller/HelloWorldController.java
+++ swagger.demo/src/main/java/com/springboot/swagger/demo/controller/HelloWorldController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/hello")
